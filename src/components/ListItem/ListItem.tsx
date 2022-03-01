3ad31a2828c6d1258a2687f8eabe9a00811da2c2
Joren Rothman <rothmanjoren@gmail.com>
2022-03-01 20:06:59 +0100

    Fixed click issue

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,4 +1,3 @@
-import { MouseEventHandler } from "react";
 import useStore from "../../store";
 
 interface Props {
@@ -10,18 +9,20 @@ interface Props {
 function ListItem({ id, text, isChecked }: Props) {
   const updateItemIsChecked = useStore((state) => state.updateItemIsChecked);
 
-  const onClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+  const onClick = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+    console.log("click");
     updateItemIsChecked(id);
   };
 
   return (
-    <li key={id} onClick={onClick} className="mb-2">
+    <li key={id} className="mb-2">
       <label className="flex items-center text-lg">
         <input
           type="checkbox"
           className="mr-2 rounded bg-gray-200 border-transparent focus:border-transparent focus:bg-gray-200 text-slate-600 focus:ring-1 focus:ring-offset-2 focus:ring-gray-500"
           checked={isChecked}
           readOnly
+          onClick={onClick}
           id={id}
         />
         <span className={isChecked ? "text-slate-600" : ""}>{text}</span>
